7c3a525f086552ab03abc9ae8cefa0c8b48e9e24
waseemKashif <waseemkashif7@gmail.com>
2023-12-25 21:55:39 +0500

    created footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/footer";
 const poppins = Poppins({
   weight: ["400", "200", "300", "500", "600", "700", "800"],
   subsets: [],
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={`${poppins.className}, antialiased`}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
